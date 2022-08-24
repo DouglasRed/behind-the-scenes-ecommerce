33d46cf8635eb66f2291539256b479bd0cf1bf0a
Doug Red <douglasred63@yahoo.com>
2022-08-23 23:12:35 -0500

    added models

--- models/index.js
+++ models/index.js
@@ -1,17 +1,29 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: "category",
+});
 // Categories have many Products
-
+Category.belongsToMany(Product, {
+  foreignKey: "category",
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: "product_tag",
+  foreignKey: "product",
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: "product_tag",
+  foreignKey: "product",
+});
 module.exports = {
   Product,
   Category,
